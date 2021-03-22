66822c122c58104c59dd90f98d94b7191795da5b
Everaldo da costa filho <everaldo.webmail@gmail.com>
2021-03-22 15:57:26 -0300

    feat(config): :construction: Criado o esqueleto do projeto symfony

--- public/index.php
+++ public/index.php
@@ -1,5 +1,22 @@
 <?php
 
-echo 'VERSÃO: PHP ' . phpversion();
-echo '<hr>';
-echo 'DATA HORA: ' . date('Y-m-d H:i:s');
+use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
+use Symfony\Component\HttpFoundation\Request;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+
+if ($_SERVER['APP_DEBUG']) {
+    umask(0000);
+
+    Debug::enable();
+}
+
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
